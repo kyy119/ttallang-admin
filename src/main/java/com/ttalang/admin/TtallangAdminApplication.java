87f72c7f8c3431e2f7c26b93c61b0dd7f363bf36
YoungYoon <duddbs1994@naver.com>
2024-10-28 10:31:26 +0900

    first commit

---
+++ src/main/java/com/ttalang/admin/TtallangAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ttalang.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtallangAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtallangAdminApplication.class, args);
+    }
+
+}
